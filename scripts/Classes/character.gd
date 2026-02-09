854f64a051ad8c9f9221de9aa783daa4521d6174
fire5kirby <157751044+fire5kirby@users.noreply.github.com>
2026-02-09 16:11:45 -0500

    Optimizations and guidelines
    Removed raycast functionality
    Added targeting constants, variable, & targeting storage objects
    Added framecounters
    Added setters & getters for framecounters and targeting variable
    Added a targeting template for children
    Added more comments

--- scripts/Classes/character.gd
+++ scripts/Classes/character.gd
@@ -1,23 +1,33 @@
 extends CharacterBody3D
 class_name Character
-##signal enable_tracking(body : CollisionObject3D)
-##signal disable_tracking(body : CollisionObject3D)
 
+###############
+## Constants ##
+###############
 ## Targeting constants
-const CLOSE = 0
-const FAR = 1
-const FIRST = 2
-const LAST = 3
-const STRONG = 4
-const WEAK = 5
+const FIRST = 0
+const LAST = 1
+const CLOSE = 2
+const STRONG = 3
+const WEAK = 4
 
-var tracking_distance: Dictionary[RayCast3D, float] = {}
-var tracking_object: Dictionary[Node3D, RayCast3D] = {}
+#####################
+## Storage Objects ##
+#####################
+
+## map and array for tracking chracters within the range
+# need to add functions to empty array and map if there are no non
+# null elements
 var tracking_dict: Dictionary[Node3D, int] = {}
 var tracking_array = []
 
-@onready var raycast_scene: PackedScene = preload("res://ray_cast_3d.tscn")
+######################
+## Global Variables ##
+######################
+## !!ONLY ACCESS THROUGH SETTERS AND GETTERS IN MOST CASES!! ##
 
+## frame trackers
+var phys_framecount = 0
 ## Movement speed multiplier for the character 
 @export var speed = 5.0
 ## Veloicty for when / if the chracter jumps
@@ -29,12 +39,38 @@ var tracking_array = []
 @export var range_detection = 1
 ## Damage multiplier for when the character deals damage to another character
 @export var damage = 1
+## The default targeting priority
+@export var target_type: int = FIRST
 
+#########################
+## Functions & Methods ##
+#########################
+
+func get_phys_framecount() -> int:
+	return phys_framecount
+
+func set_phys_framecount(new_count: int) -> bool:
+	if new_count > 60:
+		phys_framecount = 1
+		return false
+	phys_framecount = new_count
+	return true
+
+func get_target_type() -> int:
+	return target_type
+
+func set_target_type(type: int) -> bool:
+	if (type < 0 && type > 8):
+		return false
+	target_type = type
+	return true
 
 func _ready() -> void:
 	pass
-	
+
 func _physics_process(delta: float) -> void:
+	set_phys_framecount(get_phys_framecount() + 1)
+	## DEBUG LINE: print(get_phys_framecount())
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -56,21 +92,30 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	
-	var obj_list = $RangeDetection.get_overlapping_bodies()
-	## updates the position for all colliding bodies
-	for body in obj_list:
-		print(body.name + " distance from " + name + " is " + str(get_distance_char(body)))
-		tracking_dict[body] = get_distance_char(body)
+	## updates body positions every 4 frames
+	if phys_framecount % 4 == 0:
+		var obj_list = $RangeDetection.get_overlapping_bodies()
+		## updates the position for all colliding bodies
+		for body in obj_list:
+			## ignores if the body is self
+			if body.name != name:
+				print(body.name + " distance from " + name + " is " + str(get_distance_char(body)))
+				tracking_dict[body] = get_distance_char(body)
 
-#func create_raycast(character: Node3D) -> RayCast3D:
-	#var raycast = raycast_scene.instantiate()
-	#add_child(raycast)
-	#raycast.connect("detect_foe", _on_ray_cast_3d_detect_foe)
-	#raycast.connect("detect_friend", _on_ray_cast_3d_detect_friend)
-	###connect("_on_ray_cast_3d_detect_foe", raycast.detect_foe)
-	###connect("_on_ray_cast_3d_detect_friend", raycast.detect_friend)
-	#raycast.enable_tracking(character)
-	#return raycast
+## function to be overridden by children; handles the targeting
+## of chracters for attacks. 
+## this serves only as a potential example
+func target(body: Node3D) -> bool:
+	match get_target_type():
+		## targeting specfics could be handled by sub functions
+		FIRST:
+			print(name + " targeted " + body.name + " with First")
+			return true
+		LAST:
+			print(name + " targeted " + body.name + " with Last")
+			return true
+		_:
+			return false	
 
 func get_distance_char(body: Node3D) -> int:
 	return to_local(body.global_transform.origin).length()
@@ -79,31 +124,18 @@ func remove_char_array(body: Node3D) -> void:
 	tracking_array[tracking_array.find(body)] = null
 	return 
 
-#func _on_ray_cast_3d_detect_foe(body: CollisionObject3D) -> void:
-	#print(name + " detected foe " + body.name)
-#
-#func _on_ray_cast_3d_detect_friend(body: CollisionObject3D) -> void:
-	#print(name + " detected friend " + body.name)
-
 func _on_range_detection_body_exited(body: Node3D) -> void:
-	## When translating the Character and entering scene, RangeDetection will detect its own Body 
+	## When translating the Character and entering scene, 
+	## RangeDetection will detect its own Body 
 	if (body.name != name):
 		tracking_dict.erase(body)
 		remove_char_array(body)
-		#var raycast = tracking_object[body]
-		#tracking_object.erase(body)
-		#raycast.disable_tracking(body)
-		#tracking_distance.erase(raycast)
 		print(name + " no longer tracking " + body.name)
 
 func _on_range_body_entered(body: Node3D) -> void:
-	## When translating the Character adn entering scene, RangeDetection will detect its own Body 
+	## When translating the Character and entering scene, 
+	## RangeDetection will detect its own Body 
 	if (body.name != name):
 		tracking_dict[body] = get_distance_char(body)
 		tracking_array.append(body)
-		
-		#var raycast = create_raycast(body)
-		#tracking_object[body] = raycast
-		#tracking_distance[raycast] = raycast.target_position.length()
-		
 		print(name + " tracking " + body.name)
