c9742e6c9675aabed2ce31f4977dcec9e0707190
timesreading <coxkacie35@gmail.com>
2026-02-22 09:15:34 -0500

    Movement of enemies

--- scripts/Classes/character.gd
+++ scripts/Classes/character.gd
@@ -79,18 +79,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
-
+		
 	move_and_slide()
 	
 	## updates body positions every 4 frames
--- scripts/Classes/enemy.gd
+++ scripts/Classes/enemy.gd
@@ -3,9 +3,28 @@ class_name Enemy
 
 func _physics_process(delta: float) -> void:
 	super(delta)
+	movement()
 
 func _on_range_detection_body_exited(body: Node3D) -> void:
 	super(body)
 
 func _on_range_body_entered(body: Node3D) -> void:
 	super(body)
+	
+#Moves the enemy
+#Uses randf range to create 'organic' movement on the z axis, needs more smoothingg
+func movement ():
+	var variation = randf_range(.5, -.5)
+	self.position.z += lerp(variation, variation, .1)
+
+	#moves the enemy along the x axis across the screen
+	if self.position.x < 30: 
+		self.position.x += 0.1
+	elif self.position.x >= 30: 
+		self.position.x = -30
+		#puts them on the left edge of the map, off screen
+
+#Attacks towers within radius
+func attackingTowers(): 
+	pass
+	
