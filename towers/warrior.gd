6e51314742fb795323d593990ebb76882d52b0e7
Rachel-Sherrick <rachelsherrick45@gmail.com>
2026-03-07 23:31:21 -0500

    warrior targeting and turning

--- towers/warrior.gd
+++ towers/warrior.gd
@@ -4,6 +4,9 @@ class_name Warrior
 @onready var warrior_hurt_box: Area3D = $warrior_hurt_box
 @onready var hurt_box_mesh: MeshInstance3D = $warrior_hurt_box/hurt_box_mesh
 @onready var warrior_timer: Timer = $WarriorTimer
+@onready var animation_controller: Node3D = $AnimationController
+
+
 
 #is the cooldown over
 var swing_ready : bool = false
@@ -24,6 +27,8 @@ func _process(delta: float) -> void:
 		for enemy in enemy_in_range:
 			warrior_swing(enemy)
 		reset_cooldown()
+	if !($RangeDetection.has_overlapping_bodies()):
+		animation_controller.play("idle")
 
 func _physics_process(delta: float) -> void:
 	super(delta)
@@ -45,6 +50,9 @@ func _on_timer_timeout():
 #dealing warrior sword swing damage to enemies
 func warrior_swing(enemy: Enemy) -> void:
 	print(name, " attacked ", enemy.name)
+	var dir = get_direction_to_enemy(enemy)
+	var cardinal = get_cardinal_direction(dir)
+	change_attack_direction(cardinal)
 	## !! see comments in Character.gd !!
 	enemy.take_damage(1)
 	print(enemy.name + " health: ", enemy.get_health())
@@ -75,3 +83,72 @@ func target(body: Node3D) -> bool:
 			return true
 		_:
 			return false	
+			
+func get_first_enemy() -> Enemy:
+	var first = null
+	#30 taken from movement function
+	var edge = 30
+	var placeholder = 0
+	
+	for body in $RangeDetection.get_overlapping_bodies():
+		var pos = global_position
+		if pos < edge && pos > placeholder:
+			placeholder = pos
+			first = body
+	
+	return first
+	
+func get_last_enemy() -> Enemy:
+	var last = null
+	var edge = -30
+	var placeholder = 0
+	
+	for body in $RangeDetection.get_overlapping_bodies():
+		var pos = global_position
+		if pos > edge && pos < placeholder:
+			placeholder = pos
+			last = body
+	
+	return last
+	
+func get_direction_to_enemy(enemy: Node3D) -> Vector3:
+	return (enemy.global_transform.origin - global_transform.origin).normalized()
+	
+enum Direction {
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST
+}
+
+func get_cardinal_direction(dir: Vector3) -> int:
+	#if the absolute value of x is greater than z then its east to west
+	if abs(dir.x) > abs(dir.z):
+		if dir.x > 0:
+			return Direction.EAST
+		else:
+			return Direction.WEST
+	else:
+		if dir.z > 0:
+			return Direction.SOUTH
+		else:
+			return Direction.NORTH
+			
+func change_attack_direction(direction: int):
+	match direction:
+		Direction.NORTH:
+			warrior_hurt_box.rotation.y = deg_to_rad(180)
+			animation_controller.play("swing_north")
+			animation_controller.play("swing")
+		Direction.SOUTH:
+			warrior_hurt_box.rotation.y = deg_to_rad(0)
+			animation_controller.play("swing_south")
+			animation_controller.play("swing")
+		Direction.EAST:
+			warrior_hurt_box.rotation.y = deg_to_rad(90)
+			animation_controller.play("swing_east")
+			animation_controller.play("swing")
+		Direction.WEST:
+			warrior_hurt_box.rotation.y = deg_to_rad(-90)
+			animation_controller.play("swing_west")
+			animation_controller.play("swing")
