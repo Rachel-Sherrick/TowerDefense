df0c9a0d8e5d5d9d9d05d6909876f5e9febca381
fire5kirby <157751044+fire5kirby@users.noreply.github.com>
2026-03-31 17:10:34 -0400

    moved around spawning; now have to spend money to buy  stuff and spawn towers

--- player.gd
+++ player.gd
@@ -1,10 +1,16 @@
 class_name Player
 extends Node
 
+signal tower_placed(tower: Tower)
+@export var tower_scene: PackedScene
+
 ##the amount of money the player has
 @export var money: float
 ##the rate of intrest earned for clearing a wave
 @export var interest: float
+
+##the selected inventory item
+@export_enum("warrior", "buff_potion", "damage_potion") var selected_item: String
 
 ##the inventory of potions and bought player towers
 var inventory_dict: Dictionary[String, int]
@@ -28,18 +34,29 @@ func _alter_money(value: float):
 func _on_wave_cleared() -> void:
 	money *= interest
 
-# Decreases money when tower placed; temp function
-func _on_placeable_area_tower_placed(tower: Tower) -> bool:
-	if (money > 0):
-		_alter_money(-1)
-		return true
-	tower.queue_free()
-	return false
+func _place_tower(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event.is_action_pressed("click"):
+		if (
+			inventory_dict.get(selected_item) == null
+			or inventory_dict.get(selected_item) < 1
+		):
+			print("No bought ", selected_item)
+			return
+		inventory_dict[selected_item] -= 1
+		var tower: Tower = tower_scene.instantiate()
+		get_parent().get_node("Battleground").get_node("Characters").add_child(tower)
+		tower.global_position = Vector3(event_position.x, 0, event_position.z)
+		print(tower.name + " spawned at ", event_position)
+		emit_signal("tower_placed", tower)
 
 func _on_shop_purchase(item_name: String, cost: float) -> void:
 	if money - cost < 0:
 		print(name, " went out of money")
 		return
 	_alter_money(-cost)
-	inventory_dict[name] = 1
+	if (inventory_dict.get(selected_item) == null):
+		inventory_dict[item_name] = 1
+	else:
+		inventory_dict[item_name] += 1
 	print(item_name, " bought")
+	print(inventory_dict)
