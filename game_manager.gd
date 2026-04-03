a9c29be454fa67e66afe22266130e5d70770b0a5
Rachel-Sherrick <rachelsherrick45@gmail.com>
2026-04-03 18:49:44 -0400

    assets and menus
    replaced old assets with posterized assets, edited the start menu, and added a pause menu.

--- game_manager.gd
+++ game_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@onready var how_to_play_screen: CanvasLayer = $"../HowToPlayScreen"
+@onready var pause_canvas: CanvasLayer = $"../PauseCanvas"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,17 +12,17 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
-
-
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
 
-func _on_how_to_play_button_pressed() -> void:
-	how_to_play_screen.show()
+func _on_pause_button_pressed() -> void:
+	pause_canvas.show()
+	get_tree().paused = true
+	print("GAME PAUSED")
 
 
-func _on_back_button_pressed() -> void:
-	how_to_play_screen.hide()
+func _on_play_button_pressed() -> void:
+	pause_canvas.hide()
+	get_tree().paused = false
+	print("GAME RESUMED")
