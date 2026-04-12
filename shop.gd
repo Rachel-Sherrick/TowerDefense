e4d5950981fb9d24b065c9f43739c45a0b6543f8
fire5kirby <157751044+fire5kirby@users.noreply.github.com>
2026-04-12 13:53:57 -0400

    Update shop.gd

--- shop.gd
+++ shop.gd
@@ -10,39 +10,39 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _on_slot_1_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+func _on_slot_1_input_event(camera: Node, event: InputEvent, shape_idx: int) -> void:
 	if (
 		event.is_action_pressed("click") 
 	):
 		emit_signal("purchase", "warrior", 1)
 
-func _on_slot_2_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+func _on_slot_2_input_event(camera: Node, event: InputEvent, shape_idx: int) -> void:
 	if (
 		event.is_action_pressed("click") 
 	):
 		emit_signal("purchase", "wizard", 1)
 
-func _on_slot_3_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+func _on_slot_3_input_event(camera: Node, event: InputEvent, shape_idx: int) -> void:
 	if (
 		event.is_action_pressed("click") 
 	):
 		emit_signal("purchase", "archer", 1)
 
-func _on_slot_4_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+func _on_slot_4_input_event(camera: Node, event: InputEvent, shape_idx: int) -> void:
 	if (
 		event.is_action_pressed("click") 
 	):
 		emit_signal("purchase", "range_potion", 1)
 		#increases tower range
 
-func _on_slot_5_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+func _on_slot_5_input_event(camera: Node, event: InputEvent, shape_idx: int) -> void:
 	if (
 		event.is_action_pressed("click") 
 	):
 		emit_signal("purchase", "damage_potion", 1)
 		#increases tower damage points per hit
 
-func _on_slot_6_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+func _on_slot_6_input_event(camera: Node, event: InputEvent, shape_idx: int) -> void:
 	if (
 		event.is_action_pressed("click") 
 	):
