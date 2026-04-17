52ba9fafb26c0b5e626690d9a7aaaca1761c131e
fire5kirby <157751044+fire5kirby@users.noreply.github.com>
2026-04-17 16:05:57 -0400

    Merge branch 'Rachel-Branch' into Test-Branch

---
+++ scripts/Classes/character.gd
@@ -0,0 +1,143 @@
+extends CharacterBody3D
+class_name Character
+
+###############
+## Constants ##
+###############
+## Targeting constants
+const FIRST = 0
+const LAST = 1
+const CLOSE = 2
+const STRONG = 3
+const WEAK = 4
+
+#####################
+## Storage Objects ##
+#####################
+
+## data structures for tracking chracters within the range
+## first (0) is the newest body to enter
+var tracking_array : Array[CharacterBody3D] 
+
+var currentTarget
+
+######################
+## Global Variables ##
+######################
+## !!ONLY ACCESS THROUGH SETTERS AND GETTERS IN MOST CASES!! ##
+
+## frame trackers
+var phys_framecount = 0
+## Movement speed multiplier for the character 
+@export var speed = 5.0
+## Veloicty for when / if the chracter jumps
+@export var jump_velocity = 4.5
+## Range multiplier for the character's range
+## Only set range through RangeDetection's set_range
+@export var range_detection = 1
+## Damage multiplier for when the character deals damage to another character
+@export var damage = 1
+## The default targeting priority
+@export var target_type: int = FIRST
+## Arbitrary definition for strength for targeting
+@export var strength: int = 0
+
+
+#########################
+## Functions & Methods ##
+#########################
+
+func get_phys_framecount() -> int:
+	return phys_framecount
+
+func set_phys_framecount(new_count: int) -> bool:
+	if new_count > 60:
+		phys_framecount = 1
+		return false
+	phys_framecount = new_count
+	return true
+	
+func get_health() -> int:
+	return health_component.current_health
+
+## !! Change so that health
+	
+func set_health(health_lost: int) -> bool:
+	if health_component.set_current_health(health_lost):
+		return true
+	return false
+	
+	
+##returns target_type
+func get_target_type() -> int:
+	return target_type
+
+## sets targest type bases on the int provided
+func set_target_type(type: int) -> bool:
+	if (type < 0 || type >= 5):
+		return false
+	target_type = type
+	return true
+
+func _ready() -> void:
+	pass
+
+func _physics_process(_delta: float) -> void:
+	set_phys_framecount(get_phys_framecount() + 1)
+	## DEBUG LINE: print(get_phys_framecount())
+	
+	
+	move_and_slide()
+	
+	## updates body positions every 4 frames
+	if get_phys_framecount() % 4 == 0:
+		update_tracking_structures()
+
+
+##Use this to get the distance of a character
+func get_distance_char(body: Node3D) -> float:
+	return to_local(body.global_transform.origin).length()
+
+
+
+## updates the structures the object tracks
+## by rule of thumb characters do not tracks allies unless toggled
+func update_tracking_structures() -> bool:
+	## updates the position for all colliding bodies
+	var obj_list = $RangeDetection.get_overlapping_bodies()
+	## !! remove the for loop below and replace with a call to sort
+	## the distance / strength array using the Array's built-in sort_custom()!!
+	for body in obj_list:
+		## See healer.gd for old code
+		print(body.name + " distance from " + name + " is " + str(get_distance_char(body)))
+	
+	return true
+	
+
+func _on_range_detection_body_exited(body: Node3D) -> void:
+	##See healer.gd for old code
+	
+	removeTrack(body)
+	print(name + " no longer tracking " + body.name)
+	print(tracking_array)
+	
+func _on_range_body_entered(body: Node3D) -> void:
+	## See healer.gd for old code
+	#adds the body entering to the front of the array
+	addTrack(body)
+	print(tracking_array) 
+
+
+func addTrack(body):
+		if !tracking_array.has(body): 
+			tracking_array.push_front(body)
+		
+func removeTrack(body): 
+	if tracking_array.has(body): 
+		tracking_array.erase(body)
+
+##H.S added to get health to work properly
+@onready var health_component = $Health
+
+func take_damage(amount: int) -> void:
+	health_component.take_damage(amount)
---
+++ scripts/warrior.gd
@@ -0,0 +1,167 @@
+extends Tower
+class_name Warrior
+
+@onready var warrior_hurt_box: Area3D = $warrior_hurt_box
+@onready var hurt_box_mesh: MeshInstance3D = $warrior_hurt_box/hurt_box_mesh
+@onready var warrior_timer: Timer = $WarriorTimer
+@onready var animation_controller: Node3D = $AnimationController
+
+
+
+#is the cooldown over
+var swing_ready : bool = false
+
+func _ready() -> void:
+	#on load the hurt box is not visible. the mesh instance is just a visual for the hurt box
+	warrior_hurt_box.hide()
+	warrior_timer.start()
+
+func _process(delta: float) -> void:
+	#if warrior_timer.is_stopped():
+		#print("TIMER NOT WORKING")
+	if swing_ready == true && $RangeDetection.has_overlapping_bodies():
+		await attack_handler()
+	if !($RangeDetection.has_overlapping_bodies()):
+		animation_controller.play("idle")
+
+func _physics_process(delta: float) -> void:
+	super(delta)
+
+func _on_range_detection_body_exited(body: Node3D) -> void:
+	super(body)
+
+func _on_range_body_entered(body: Node3D) -> void:
+	super(body)
+	#if swing_ready:
+	#	attack_handler()
+	
+func _on_timer_timeout():
+	swing_ready = true
+	print(name, "'s ATTACK READY")
+
+##handles the attacking of enemies
+func attack_handler() -> void:
+		reset_cooldown()
+		var enemy_in_range = warrior_hurt_box.get_overlapping_bodies()
+		## targets an enemy; does not change target if there is no enemy returned
+		attack_alter_area(target())
+		show_hurt_box()
+		for enemy in enemy_in_range:
+			attack_damage(enemy)
+		##forces attacks to wait till animation is finished till a new one begins
+		await animation_controller.animation_finished
+
+## Handles the animation and the oritentation of an attack
+func attack_alter_area(target: Enemy) -> void:
+	if target == null:
+		return
+	var dir = get_direction_to_enemy(target)
+	var cardinal = get_cardinal_direction(dir)
+	change_attack_direction(cardinal)
+
+#dealing warrior sword swing damage to enemies
+func attack_damage(enemy: Enemy) -> void:
+	print(name, " attacked ", enemy.name)
+	## !! see comments in Character.gd !!
+	enemy.take_damage(1)
+	print(enemy.name + " health: ", enemy.get_health())
+
+#reset timer and swing ready
+func reset_cooldown() -> void:
+	warrior_timer.start()
+	swing_ready = false
+	print(name + "'s attack on cooldown")
+
+#show hurt box for 1 second when the warrior swings
+func show_hurt_box() -> void:
+	warrior_hurt_box.show()
+	print(name + "'s hurt box shown")
+	## this line will wait 3 sec before proceeding to the next
+	## altered to wait for the animation to finish
+	await get_tree().create_timer(0.5).timeout
+	warrior_hurt_box.hide()
+	print(name + "'s hurt box hidden")
+	
+## Needs to be altered to access the tracking array 
+## Use target() (see character.gd) instead
+func get_first_enemy() -> Enemy:
+	var first = null
+	#30 taken from movement function
+	var edge = 30
+	var placeholder = 0
+	
+	for body in $RangeDetection.get_overlapping_bodies():
+		var pos = global_position
+		if pos < edge && pos > placeholder:
+			placeholder = pos
+			first = body
+	
+	return first
+
+## Needs to be altered to access the tracking array 
+## Use target() (see character.gd) instead
+func get_last_enemy() -> Enemy:
+	var last = null
+	var edge = -30
+	var placeholder = 0
+	
+	for body in $RangeDetection.get_overlapping_bodies():
+		var pos = global_position
+		if pos > edge && pos < placeholder:
+			placeholder = pos
+			last = body
+	
+	return last
+	
+func get_direction_to_enemy(enemy: Node3D) -> Vector3:
+	return (enemy.global_transform.origin - global_transform.origin).normalized()
+	
+enum Direction {
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST
+}
+
+func get_cardinal_direction(dir: Vector3) -> int:
+	#if the absolute value of x is greater than z then its east to west
+	if abs(dir.x) > abs(dir.z):
+		if dir.x > 0:
+			return Direction.EAST
+		else:
+			return Direction.WEST
+	else:
+		if dir.z > 0:
+			return Direction.SOUTH
+		else:
+			return Direction.NORTH
+			
+func change_attack_direction(direction: int):
+	match direction:
+		Direction.NORTH:
+			warrior_hurt_box.rotation.y = deg_to_rad(180)
+			animation_controller.play("swing_north")
+			#animation_controller.play("swing")
+		Direction.SOUTH:
+			warrior_hurt_box.rotation.y = deg_to_rad(0)
+			animation_controller.play("swing_south")
+			#animation_controller.play("swing")
+		Direction.EAST:
+			warrior_hurt_box.rotation.y = deg_to_rad(90)
+			animation_controller.play("swing_east")
+			#animation_controller.play("swing")
+		Direction.WEST:
+			warrior_hurt_box.rotation.y = deg_to_rad(-90)
+			animation_controller.play("swing_west")
+			#animation_controller.play("swing")
+func target():
+	match get_target_type():
+		## targeting specfics could be handled by sub functions
+		##Warrier specific targeting goes here.
+		FIRST:
+			pass
+			
+		LAST:
+			pass
+		_:
+			return false	
