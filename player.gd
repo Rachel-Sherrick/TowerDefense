67dd73fa558d67b82e2237c9c2669fda5f70e5cd
Rachel-Sherrick <rachelsherrick45@gmail.com>
2026-04-23 23:20:25 -0400

    inventory tower placement
    also how to play screen

--- player.gd
+++ player.gd
@@ -2,7 +2,8 @@ class_name Player
 extends Node
 
 signal tower_placed(tower: Tower)
-@export var tower_scene: PackedScene
+@export var warrior_scene: PackedScene
+@export var wizard_scene: PackedScene
 
 ##the amount of money the player has
 @export var money: float
@@ -15,6 +16,11 @@ signal tower_placed(tower: Tower)
 var warrior_num: int = 0
 @onready var wizard_count: Label = $GUI/InventoryBar/WizardBox/WizardCount
 var wizard_num: int = 0
+
+@onready var warrior_box: ColorRect = $GUI/InventoryBar/WarriorBox
+@onready var wizard_box: ColorRect = $GUI/InventoryBar/WizardBox
+#var warrior_selected: bool = false
+#var wizard_selected: bool = false
 
 ##the selected inventory item
 @export_enum("warrior", "wizard") var selected_item: String
@@ -60,18 +66,30 @@ func _place_tower(camera: Node, event: InputEvent, event_position: Vector3, norm
 		inventory_dict[selected_item] -= 1
 		
 		##updates the text
-		##should be moved to its own function
-		if (inventory_dict.get("warrior") != null):
-			warrior_count.text = (str(inventory_dict["warrior"]))
-		if (inventory_dict.get("wizard") != null):
-			wizard_count.text = (str(inventory_dict["wizard"]))
+		update_inventory_text()
 		
 		## spawns the selected tower
-		var tower: Tower = tower_scene.instantiate()
-		get_parent().get_node("Battleground").get_node("Characters").add_child(tower)
-		tower.global_position = Vector3(event_position.x, 0, event_position.z)
-		print(tower.name + " spawned at ", event_position)
-		emit_signal("tower_placed", tower)
+		if selected_item == "warrior":
+			var tower: Tower = warrior_scene.instantiate()
+			get_parent().get_node("Battleground").get_node("Characters").add_child(tower)
+			tower.global_position = Vector3(event_position.x, 0, event_position.z)
+			print(tower.name + " spawned at ", event_position)
+			emit_signal("tower_placed", tower)
+			warrior_box.color = Color("fed863")
+			
+		if selected_item == "wizard":
+			var tower: Tower = wizard_scene.instantiate()
+			get_parent().get_node("Battleground").get_node("Characters").add_child(tower)
+			tower.global_position = Vector3(event_position.x, 0, event_position.z)
+			print(tower.name + " spawned at ", event_position)
+			emit_signal("tower_placed", tower)
+			wizard_box.color = Color("fed863")
+			
+		#var tower: Tower = tower_scene.instantiate()
+		#get_parent().get_node("Battleground").get_node("Characters").add_child(tower)
+		#tower.global_position = Vector3(event_position.x, 0, event_position.z)
+		#print(tower.name + " spawned at ", event_position)
+		#emit_signal("tower_placed", tower)
 
 func _on_shop_purchase(item_name: String, cost: float) -> void:
 	if money - cost < 0:
@@ -80,19 +98,43 @@ func _on_shop_purchase(item_name: String, cost: float) -> void:
 	_alter_money(-cost)
 	if (inventory_dict.get(item_name) == null):
 		inventory_dict[item_name] = 1
-		if item_name == "warrior":
-			warrior_num = inventory_dict[item_name]
-			warrior_count.text = (str(warrior_num))
-		elif item_name == "wizard":
-			wizard_num = inventory_dict[item_name]
-			wizard_count.text = (str(wizard_num))
+		inventory_purchase_update(item_name)
 	else:
 		inventory_dict[item_name] += 1
-		if item_name == "warrior":
-			warrior_num = inventory_dict[item_name]
-			warrior_count.text = (str(warrior_num))
-		elif item_name == "wizard":
-			wizard_num = inventory_dict[item_name]
-			wizard_count.text = (str(wizard_num))
+		inventory_purchase_update(item_name)
 	print(item_name, " bought")
 	print(inventory_dict)
+	
+func update_inventory_text() -> void:
+	if (inventory_dict.get("warrior") != null):
+		warrior_count.text = (str(inventory_dict["warrior"]))
+	if (inventory_dict.get("wizard") != null):
+		wizard_count.text = (str(inventory_dict["wizard"]))
+
+func inventory_purchase_update(tower_purchase) -> void:
+	if tower_purchase == "warrior":
+		warrior_num = inventory_dict[tower_purchase]
+		warrior_count.text = (str(warrior_num))
+	elif tower_purchase == "wizard":
+		wizard_num = inventory_dict[tower_purchase]
+		wizard_count.text = (str(wizard_num))
+
+func _on_warrior_select_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if (event.is_action_pressed("click")) && selected_item == "wizard":
+		#print("CLICKED")
+		warrior_box.color = Color("fff1ca")
+		wizard_box.color = Color("fed863")
+		selected_item = "warrior"
+	elif (event.is_action_pressed("click")):
+		#print("CLICKED")
+		warrior_box.color = Color("fff1ca")
+		selected_item = "warrior"
+
+func _on_wizard_select_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if (event.is_action_pressed("click")) && selected_item == "warrior":
+		wizard_box.color = Color("fff1ca")
+		warrior_box.color = Color("fed863")
+		selected_item = "wizard"
+	elif (event.is_action_pressed("click")):
+		wizard_box.color = Color("fff1ca")
+		selected_item = "wizard"
