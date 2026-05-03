e49f2f8a50062062ed85b339b4f8df493a7ee71c
fire5kirby <157751044+fire5kirby@users.noreply.github.com>
2026-05-03 09:47:25 -0400

    polished rounds

--- Scenes/Game Scenes/enemy_spawner.gd
+++ Scenes/Game Scenes/enemy_spawner.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+
 @export var enemy_scene: PackedScene
 @export var spawn_point: Node3D
 @export var spawn_interval: float = 4.0   # longer delay after first
@@ -7,43 +8,48 @@ extends Node3D
 @onready var final_target = get_tree().current_scene.get_node("Battleground/Orb")
 var spawn_interval_done: bool = false
 var round_started: bool = false
-
 @onready var start_round_button: Button = $"../Player/GUI/StartRoundButton"
 var wave = 1
 var round = 1
-@onready var wave_label: Label = $Player/GUI/WaveLabel
-@onready var round_label: Label = $Player/GUI/RoundLabel
-@onready var health: Health = $Health
+@onready var wave_label: Label = $"../Player/GUI/WaveLabel"
+@onready var round_label: Label = $"../Player/GUI/RoundLabel"
 var enemies_killed = 0
-
-
 var _timer: float = 0.0
 var _count: int = 0
+var orb_dead: bool = false
+
+signal round_completed()
 
 func _ready() -> void:
 	print("Spawner ready")
 	$SpawnTimer.wait_time = spawn_interval
-	#$Battleground/Orb/Health.enemy_killed.connect(_on_enemy_killed)
+	$"../Battleground/Characters".child_exiting_tree.connect(_character_killed)
+	$"../Battleground/Orb".orb_destroyed.connect(_on_player_died)
 
 func _process(delta: float) -> void:
-	if enemies_killed >= 10 and !$Battleground/Orb.orb_destroyed:
-		if wave < 5:
+	pass
+
+func advance_wave():
+	if enemies_killed >= 10 and !(orb_dead):
+		if wave < 5 and round >= 3:
 			wave += 1
+			round = 1
 			wave_label.text = ("Wave: " + str(wave))
 		else: 
-			wave = 1
 			round += 1
 			round_label.text = ("Round: " + str(round))
 			start_round_button.show()
 			round_started = false
+			emit_signal("round_completed")
 
 func spawn_enemy() -> void:
 	var enemy: Enemy = enemy_scene.instantiate()
 	enemy.final_target = final_target
-	var parent = get_tree().current_scene.get_node("Battleground/Characters")
-	parent.add_child(enemy)
+	var parent = get_tree().current_scene.get_node("Battleground").get_node("Characters")
 	spawn_point.position.z = randi_range(-12, 12)
-	enemy.global_position = spawn_point.global_position
+	enemy.position.x = -30
+	parent.add_child(enemy)
+	enemy.position = spawn_point.global_position
 
 	print("Spawned at:", enemy.global_position)
 	_count += 1
@@ -73,5 +79,10 @@ func _on_start_round_button_pressed() -> void:
 	round_started = true
 	start_round_button.hide()
 	
-func _on_enemy_killed():
-	enemies_killed += 1
+func _character_killed(character):
+	if character is Enemy:
+		enemies_killed += 1
+	advance_wave()	
+		
+func _on_player_died():
+	orb_dead = true
